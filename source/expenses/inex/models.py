f8bd573d383c7c1fefca3239f66f9e05a2422302
me <rosllarr@tutanota.com>
2023-04-28 12:55:44 +0700

    update Expenses model

--- source/expenses/inex/models.py
+++ source/expenses/inex/models.py
@@ -3,10 +3,14 @@ from django.db import models
 
 class Expenses(models.Model):
     title = models.CharField(max_length=200)
-    reserved_transection_date = models.DateField('date reserved')
-    transection_date = models.DateField('action date')
-    income = models.DecimalField('income', max_digits=7, decimal_places=7)
-    expenses = models.DecimalField('expenses', max_digits=7, decimal_places=7)
+    reserved_transection_date = models.DateField('date reserved', default=None)
+    transection_date = models.DateField('action date', default=None)
+    income = models.DecimalField(
+        'income', max_digits=7, decimal_places=0, default=0)
+    expenses = models.DecimalField(
+        'expenses', max_digits=7, decimal_places=0, default=0)
+    reserved_expenses = models.DecimalField(
+        'reserved expenses', max_digits=7, decimal_places=0, default=0)
 
     def __str__(self):
         return self.title

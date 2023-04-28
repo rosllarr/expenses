fc9238cf0594aaf97146d34fe960d1f9551219f9
me <rosllarr@tutanota.com>
2023-04-28 12:08:20 +0700

    create Expenses model

--- source/expenses/expenses/settings.py
+++ source/expenses/expenses/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'inex.apps.InexConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- source/expenses/inex/models.py
+++ source/expenses/inex/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Expenses(models.Model):
+    reserved_transection_date = models.DateField('date reserved')
+    transection_date = models.DateField('action date')
+    income = models.DecimalField('income', max_digits=7, decimal_places=7)
+    expenses = models.DecimalField('expenses', max_digits=7, decimal_places=7)

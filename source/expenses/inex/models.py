66a282f84ca425bcd1bdcf3c34c3ccf843859f04
me <rosllarr@tutanota.com>
2023-04-28 12:25:30 +0700

    recreate Expenses model

--- source/expenses/inex/models.py
+++ source/expenses/inex/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 
 class Expenses(models.Model):
+    title = models.CharField(max_length=200)
     reserved_transection_date = models.DateField('date reserved')
     transection_date = models.DateField('action date')
     income = models.DecimalField('income', max_digits=7, decimal_places=7)
     expenses = models.DecimalField('expenses', max_digits=7, decimal_places=7)
+
+    def __str__(self):
+        return self.title

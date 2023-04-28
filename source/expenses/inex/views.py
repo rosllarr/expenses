d4e970026af37703ab138cfcfce77bd1d2a6748f
me <rosllarr@tutanota.com>
2023-04-28 13:31:18 +0700

    add index view

--- source/expenses/inex/views.py
+++ source/expenses/inex/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+from .models import Expenses
+
+
+def index(request):
+    # Show table according to the current month.
+    today = date.today()
+    current_month = today.month
+
+    entry_list = Expenses.objects.filter(
+        reserved_transection_date__month=current_month
+        ).order_by('reserved_transection_date')
+
+    return render(request, 'inex/index.html', {'entry_list': entry_list})

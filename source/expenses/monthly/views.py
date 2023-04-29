3e051028db627551d04db7461e1adaf4c088d87d
me <rosllarr@tutanota.com>
2023-04-29 20:36:49 +0700

    add entry_of_monthly_savings and savings tables to yearly page

--- source/expenses/monthly/views.py
+++ source/expenses/monthly/views.py
@@ -14,6 +14,37 @@ def yearly(request, year_id):
     start_date = date(year-1, 12, 25)
     end_date = date(year, 11, 25)
 
+    entry_of_monthly_savings = Monthly.objects.filter(
+        transection_date__gte=start_date,
+        transection_date__lte=end_date,
+        tags__name='year').filter(tags__name='savings')
+
+    monthly_savings_of_year = Monthly.objects.filter(
+        transection_date__gte=start_date,
+        transection_date__lte=end_date,
+        tags__name='year').filter(tags__name='savings'
+                                  ).aggregate(Sum('cost'))['cost__sum']
+
+    savings_in_year = Monthly.objects.filter(
+        transection_date__gte=start_date,
+        transection_date__lte=end_date,
+        tags__name='year'
+        ).filter(tags__name='savings'
+                 ).exclude(tags__name='car_insurance'
+                           ).aggregate(Sum('cost'))['cost__sum']
+
+    previous_savings = Monthly.objects.filter(tags__name='previous_savings'
+                                              ).values().get()['cost']
+
+    total_savings = savings_in_year + previous_savings
+
+    car_insurance = Monthly.objects.filter(
+        transection_date__gte=start_date,
+        transection_date__lte=end_date,
+        tags__name='car_insurance').aggregate(Sum('cost'))['cost__sum']
+
+    total_savings_with_car_insurance = total_savings + car_insurance
+
     entry_of_expenses = Monthly.objects.filter(
         transection_date__gte=start_date,
         transection_date__lte=end_date,
@@ -26,6 +57,13 @@ def yearly(request, year_id):
                                   ).aggregate(Sum('cost'))['cost__sum']
 
     contexts = {
+        'entry_of_monthly_savings': entry_of_monthly_savings,
+        'monthly_savings_of_year': monthly_savings_of_year,
+        'savings_in_year': savings_in_year,
+        'previous_savings': previous_savings,
+        'total_savings': total_savings,
+        'car_insurance': car_insurance,
+        'total_savings_with_car_insurance': total_savings_with_car_insurance,
         'entry_of_expenses': entry_of_expenses,
         'expenses_of_year': expenses_of_year
     }

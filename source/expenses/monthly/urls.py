29fd3d0a490df9665fe9b1978dda44e205ca746f
me <rosllarr@tutanota.com>
2023-04-29 18:21:14 +0700

    show expenses_of_year in yearly page

--- source/expenses/monthly/urls.py
+++ source/expenses/monthly/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'monthly'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:month_id>/', views.monthly, name='monthly')
+    path('<int:month_id>/', views.monthly, name='monthly'),
+    path('year/<int:year_id>/', views.yearly, name='yearly')
 ]
--- source/expenses/monthly/views.py
+++ source/expenses/monthly/views.py
@@ -2,8 +2,35 @@ from django.shortcuts import redirect, render
 from datetime import date
 from django.db.models import Sum
 from django.urls import reverse
+from django.db.models import Q
 
 from .models import Monthly
+
+
+def yearly(request, year_id):
+    # Show table according to the year
+    year = year_id
+
+    start_date = date(year-1, 12, 25)
+    end_date = date(year, 11, 25)
+
+    entry_of_expenses = Monthly.objects.filter(
+        transection_date__gte=start_date,
+        transection_date__lte=end_date,
+        tags__name='year').filter(tags__name='expenses')
+
+    expenses_of_year = Monthly.objects.filter(
+        transection_date__gte=start_date,
+        transection_date__lte=end_date,
+        tags__name='year').filter(tags__name='expenses'
+                                  ).aggregate(Sum('cost'))['cost__sum']
+
+    contexts = {
+        'entry_of_expenses': entry_of_expenses,
+        'expenses_of_year': expenses_of_year
+    }
+
+    return render(request, 'monthly/yearly.html', contexts)
 
 
 def monthly(request, month_id):
